6c9e8bec8b688504bc44dc8f28e504b467728691
gnanendrart <grt4@illinois.edu>
2019-11-06 17:02:24 -0600

    changes from deploy coding assignment part1 & 2

--- tugu_gnanendrareddy_ez_university/settings/base.py
+++ tugu_gnanendrareddy_ez_university/settings/base.py
@@ -55,7 +55,7 @@ ROOT_URLCONF = 'tugu_gnanendrareddy_ez_university.urls'
 TEMPLATES = [
     {
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
-        'DIRS': [os.path.join(BASE_DIR, 'courseinfo/templates')]
+        'DIRS': [os.path.join(BASE_DIR, 'courseinfo/../../courseinfo/templates')]
         ,
         'APP_DIRS': True,
         'OPTIONS': {
@@ -77,7 +77,7 @@ WSGI_APPLICATION = 'tugu_gnanendrareddy_ez_university.wsgi.application'
 DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+        'NAME': os.path.join(BASE_DIR, '../../db.sqlite3'),
     }
 }
 
@@ -116,7 +116,7 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/2.2/howto/static-files/
 
 STATIC_URL = '/static/'
-STATIC_ROOT = os.path.join(BASE_DIR, 'courseinfo/static/../static')
+STATIC_ROOT = os.path.join(BASE_DIR, 'courseinfo/static/../../../courseinfo/static')
 
 LOGIN_REDIRECT_URL = 'about_urlpattern'
 
