60d019a5ff6846722ed5bdb4d69fcc3d9db2cc3e
gnanendrart <grt4@illinois.edu>
2019-11-20 20:34:40 -0600

    Changes made before deployment

--- tugu_gnanendrareddy_ez_university/settings/base.py
+++ tugu_gnanendrareddy_ez_university/settings/base.py
@@ -24,9 +24,9 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 SECRET_KEY = 'y#mmv#x78nh2i)t2chr9-#-wyzhon@&pii=(-f7nj2u8a4&&j('
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['localhost', '127.0.0.1', 'gnanendrart.pythonanywhere.com']
 
 # Application definition
 
